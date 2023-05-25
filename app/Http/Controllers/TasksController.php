aa4f61b519192c1011562ba4a70a670dfa2992cc
takuma.kawakami <kawataku.org@gmail.com>
2023-05-25 04:35:57 +0000

    CRUD tasks

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Task;
+
+class TasksController extends Controller
+{
+    public function index()
+    {
+        // メッセージ一覧を取得
+        $tasks = Task::all();        
+
+        // メッセージ一覧ビューでそれを表示
+        return view('tasks.index', [     
+            'tasks' => $tasks,        
+        ]);                                 
+    }
+
+    public function create()
+    {
+        $task = new Task;
+        return view('tasks.create', ['task' => $task, ]);
+    }
+
+    public function store(Request $request)
+    {
+        $task = new Task;
+        $task->content = $request->content;
+        $task->save();
+        return redirect('/');
+    }
+    
+    public function show($id)
+    {
+        $task = Task::findOrFail($id);
+
+        return view('tasks.show', [
+            'task' => $task,
+        ]);
+    }
+
+    public function edit($id)
+    {
+        $task = Task::findOrFail($id);
+
+        return view('tasks.edit', [
+            'task' => $task,
+        ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $task = Task::findOrFail($id);
+        $task->content = $request->content;
+        $task->save();
+
+        return redirect('/');
+    }
+
+    public function destroy($id)
+    {
+        $task = Task::findOrFail($id);
+        $task->delete();
+
+        return redirect('/');
+    }
+}
